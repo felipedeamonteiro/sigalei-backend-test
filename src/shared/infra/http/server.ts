d53c6c49add255226aac3ce855f3343be7579090
Felipe Monteiro <felipedeamonteiro@gmail.com>
2020-12-11 22:28:51 -0300

    Connect database and reconfigure server

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,9 +1,14 @@
-import 'reflect-metadata';
+// import 'reflect-metadata';
 
 import express from 'express';
 import routes from './routes';
 
+import '../typeorm';
+
 const app = express();
+
+app.use(express.json());
+app.use(routes);
 
 app.get('/', (request, response) => {
   return response.json({ message: 'Hello World Porra!' });
